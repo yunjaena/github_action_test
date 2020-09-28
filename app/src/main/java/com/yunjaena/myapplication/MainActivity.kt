fceea9ab2b932731c3f68d5f06cae28300ba45d9
Yunjae-Na <yunjaena@gmail.com>
2020-09-29 04:23:49 +0900

    Add correct code
    Add correct code

--- app/src/main/java/com/yunjaena/myapplication/MainActivity.kt
+++ app/src/main/java/com/yunjaena/myapplication/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.yunjaena.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +9,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sub(a: Int, b: Int): Int = a - b
 }
